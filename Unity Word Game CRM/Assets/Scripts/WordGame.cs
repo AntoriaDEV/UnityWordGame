16d3ac53d3f94414cd311526a55129902094408d
AntoriaDEV <mitchell.christopher@aol.com>
2026-02-20 01:32:55 -0600

    WIP

--- Unity Word Game CRM/Assets/Scripts/WordGame.cs
+++ Unity Word Game CRM/Assets/Scripts/WordGame.cs
@@ -4,18 +4,37 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Versioning;
 
 public class WordGame : MonoBehaviour
 {
+    [SerializeField] TMP_InputField guessChar;
+    [SerializeField] Button submit;
+    [SerializeField] Button reset;
+    [SerializeField] TMP_Text guessesRemaining;
+    [SerializeField] TMP_Text guess;
+    [SerializeField] TextAsset wordList;
+    string correctWord = "";
+    string charGuess = "";
+    string[] lettersGuessed;
+    //string[] listOfWords = wordList.text.Split('\n');
+
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log(wordList);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadWords()
+    {
+
     }
 }
